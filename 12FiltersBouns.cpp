58b1186e15672846837c15042282a09881398661
shahdkh2288 <101909725+shahdkh2288@users.noreply.github.com>
2022-04-23 08:22:31 +0200

    Added Merge Filter"RGB"

--- 12FiltersBouns.cpp
+++ 12FiltersBouns.cpp
@@ -8,13 +8,35 @@ using namespace std;
 unsigned char colored_image[SIZE][SIZE][RGB];
 unsigned char image[SIZE][SIZE];
 unsigned char grey_image[SIZE][SIZE];
+unsigned char image2[SIZE][SIZE][RGB];
 void loadImage ();
-
 void BlackAndWhite ();
+void readnewimage();
+void Merge_Images();
+void again();
 int main(){
+    cout << "Ahlan ya user ya habibi :)" << endl;
     loadImage();
-    BlackAndWhite();
+    string f;   //to make user choose any filter as he likes
+    cout<<"Please select a filter to apply or 0 to exit: "<< endl <<"1- Black & White Filter" << endl << "3- Merge Filter"<< endl;
+    cin>>f;
+    if(f == "1"){
+        BlackAndWhite();
+    }else if(f == "3"){
+        Merge_Images();
+    }
+    again();
     return 0;
+}
+void again() {
+    string again;  //to make user do more than one filter
+    cout << "you want another filter? " << endl;
+    cin >> again;
+    while (again == "yes") {
+        main();
+        break;
+
+    }
 }
 void loadImage () {
     char imageFileName[100];
@@ -53,5 +75,34 @@ void BlackAndWhite(){
     // Add to it .bmp extension and load image
     strcat (imageFileName, ".bmp");
     writeGSBMP(imageFileName, image);
+
+}
+void readnewimage(){
+    char imageFileName[100];
+    cout << "Enter the other image file name: ";       // Get gray scale image file name
+    cin >> imageFileName;
+    strcat (imageFileName, ".bmp");     // Add to it .bmp extension and load image
+    readRGBBMP(imageFileName, image2);
+
+}
+void Merge_Images(){
+    readnewimage();
+    for (int i = 0; i < 256; i ++) {
+        for (int j = 0;j < 256; j ++) {
+            for(int y = 0;y< RGB; y ++){
+                colored_image[i][j][y]+=(image2[i][j][y])/2;
+            }
+
+        }
+    }
+    char imageFileName[100];
+
+    // Get gray scale image target file name
+    cout << "Enter the target image file name: ";
+    cin >> imageFileName;
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    writeRGBBMP(imageFileName, colored_image);
 
 }
