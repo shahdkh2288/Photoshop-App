540d6829797c8ce8bc013fae12528b6a9e34687b
shahdkh2288 <101909725+shahdkh2288@users.noreply.github.com>
2022-04-23 09:33:58 +0200

    Added Mirror 1/2 Image Filter"RGB"

--- 12FiltersBouns.cpp
+++ 12FiltersBouns.cpp
@@ -18,13 +18,18 @@ void Flip();
 void HorizontalFlip();
 void VerticalFlip();
 void DetectEdges();
+void Mirror();
+void LeftMirror();
+void RightMirror();
+void UpperMirror();
+void DownMirror();
 void again();
 int main(){
     cout << "Ahlan ya user ya habibi :)" << endl;
     loadImage();
     string f;   //to make user choose any filter as he likes
     cout<<"Please select a filter to apply or 0 to exit: "<< endl <<"1- Black & White Filter" << endl << "3- Merge Filter"<< endl <<"4- Flip Image" << endl
-            <<"7- Detect Image Edges" << endl << "0.Exit" << endl;
+            <<"7- Detect Image Edges" << endl <<"a- Mirror 1/2 Image" << endl << "0.Exit" << endl;
     cin>>f;
     if(f == "1"){
         BlackAndWhite();
@@ -34,6 +39,8 @@ int main(){
         Flip();
     }else if(f == "7"){
         DetectEdges();
+    }else if(f == "a"){
+        Mirror();
     }else if(f == "0"){
         return 0;
     }
@@ -215,5 +222,104 @@ void DetectEdges(){
     strcat (imageFileName, ".bmp");
     writeGSBMP(imageFileName, image3);
 
+
+}
+void Mirror(){
+    char letter;
+    cout<<"(l)eft" << endl << "(r)ight" << endl << "(u)pper" << endl << "(d)own side" <<endl;
+    cin>>letter;  //user must enter one of these letters "l">>leftmirror","r">>rightmirror","u">>uppermirror","d">>downmirror
+    for(int i=0;i<5;i++) {
+        if (letter == 'l') {
+            LeftMirror();
+            break;
+        } else if (letter == 'r') {
+            RightMirror();
+            break;
+        } else if (letter == 'u') {
+            UpperMirror();
+            break;
+        } else if (letter == 'd') {
+            DownMirror();
+            break;
+        } else {
+            cout << "please try again and enter one of this letters" << endl;
+            main();
+            break;
+        }
+    }
+
+}
+void LeftMirror(){
+    for(int i=0;i<SIZE;i++){
+        for(int j=SIZE/2;j<SIZE;j++){
+            for(int k=0;k<3;k++){
+                colored_image[i][j][k]=colored_image[i][SIZE-1-j][k];
+            }
+        }
+    }
+    char imageFileName[100];
+
+    // Get gray scale image target file name
+    cout << "Enter the target image file name: ";
+    cin >> imageFileName;
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    writeRGBBMP(imageFileName, colored_image);
+}
+void RightMirror(){
+    for(int i=0;i<SIZE;i++){
+        for(int j=0;j<SIZE/2;j++){
+            for(int k=0;k<3;k++){
+                colored_image[i][j][k]=colored_image[i][SIZE-j-1][k];
+            }
+        }
+    }
+    char imageFileName[100];
+
+    // Get gray scale image target file name
+    cout << "Enter the target image file name: ";
+    cin >> imageFileName;
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    writeRGBBMP(imageFileName, colored_image);
+
+}
+void UpperMirror(){
+    for(int i=SIZE/2;i<SIZE;i++){
+        for(int j=0;j<SIZE;j++){
+            for(int k=0;k<3;k++){
+                colored_image[i][j][k]=colored_image[SIZE-1-i][j][k];
+            }
+        }
+    }
+    char imageFileName[100];
+
+    // Get gray scale image target file name
+    cout << "Enter the target image file name: ";
+    cin >> imageFileName;
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    writeRGBBMP(imageFileName, colored_image);
+}
+void DownMirror(){
+    for(int i=0;i<SIZE/2;i++){
+        for(int j=0;j<SIZE;j++){
+            for(int k=0;k<3;k++){
+                colored_image[i][j][k]=colored_image[SIZE-i-1][j][k];
+            }
+        }
+    }
+    char imageFileName[100];
+
+    // Get gray scale image target file name
+    cout << "Enter the target image file name: ";
+    cin >> imageFileName;
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    writeRGBBMP(imageFileName, colored_image);
 
 }
